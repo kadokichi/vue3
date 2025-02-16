7febbbdd54334c6c738d5ac0695b1ddff08d4af3
github kadokichi <dydy2929@yahoo.co.jp>
2025-02-16 20:48:52 +0900

    getter,setter

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello Vue.js'
+    basePrice: ''
   }),
 	computed: {
-		reversedMessage: function() {
-			return this.message.split('').reverse().join('')
-		}
-	},
-	methods: {
-		reversedMessageMethod: function() {
-			return this.message.split('').reverse().join('')
+		taxIncludePrice: {
+			get: function() {
+				return this.basePrice * 1.1
+			},
+			set: function(value) {
+				this.basePrice = value / 1.1
+			}
 		}
 	}
 })
