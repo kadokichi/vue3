55c7a0898bb2a2ecaa363c33dc157b2ea4df8169
github kadokichi <dydy2929@yahoo.co.jp>
2025-02-16 16:48:10 +0900

    v-once

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
 const app = Vue.createApp({
   data: () => ({
-    newItem: '',
-		todos: []
+    message: 'Hello Vue.js'
   }),
 	methods: {
-		addItem: function(event) {
-			if(this.newItem === '') return
-			let todo = {
-				item: this.newItem,
-				isDone: false
-			}
-			this.todos.push(todo)
-			this.newItem = ''
-		},
-		deleteItem: function(index) {
-			this.todos.splice(index, 1)
+		clickHandler: function(event) {
+			this.message = this.message.split('').reverse().join('')
 		}
 	}
 })
