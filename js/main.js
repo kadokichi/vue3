02420c16e5313b89f40a833de90acaa55d2057fc
github kadokichi <dydy2929@yahoo.co.jp>
2025-02-22 21:01:09 +0900

    トランジション

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
-const buttonCounter = {
-	template: '<div><span>count: </span><button v-on:click="countUp">{{ count }}</button></div>',
-	data: () => ({
-		count: 0
-	}),
-	methods: {
-		countUp: function(event) {
-			this.count++
-		}
-	}
-}
-
 const app = Vue.createApp({
   data: () => ({
-    
-  }),
-	components: {
-		'button-counter': buttonCounter
-	}
+    show: true
+  })
 })
 app.mount('#app')
